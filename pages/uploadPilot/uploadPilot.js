e7f02640884b118a84541df5af20889fcd9954d6
1309723614 <l>
2017-10-22 21:28:17 +0800

    修改uploadpilot

--- pages/uploadPilot/uploadPilot.js
+++ pages/uploadPilot/uploadPilot.js
@@ -60,6 +60,7 @@ Page({
                                                     bookInfo: res.data,
                                                 })
                                                 var bookData = that.data.bookInfo;
+                                                var title = bookData.title? bookData.title:'';
                                                 wx.request({
                                                     url: 'https://' + app.globalData.apiUrl + '?m=home&c=Api&a=uploadBookInfo&book_name=' + bookData.title + "&writer=" + bookData.author[0]+ "&translator=" + bookData.translator[0] + "&introduction=" + bookData.summary + "&book_image=" + bookData.image + "&book_sort=" + bookData.tags[0] + "&ISBN10=" + bookData.isbn10 + "&book_press=" + bookData.publisher + "&publish_date=" + bookData.pubdate + "&web_url=" + bookData.url + "&rating=" + bookData.rating.average + "&writer_intro=" + bookData.author_intro + "&image_large=" + bookData.images.large + "&image_medium=" + bookData.images.medium + "&image_small=" + bookData.images.small + "&ISBN13=" + bookData.isbn13 + "&pages=" + bookData.pages + "&price=" + parseInt(bookData.price) + "&rating_max=" + bookData.rating.max + "&rating_min=" + bookData.rating.min + "&raters_num=" + bookData.rating.numRaters + "&subtitle=" + bookData.subtitle,
                                                     method: "GET",
