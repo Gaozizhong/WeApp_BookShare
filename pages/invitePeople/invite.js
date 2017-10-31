2a5374e03cc6fbefc1d7a1350115b59f0ea289aa
marspalLiu <1098846196@qq.com>
2017-10-31 17:33:40 +0800

    刘鑫提交

--- pages/invitePeople/invite.js
+++ pages/invitePeople/invite.js
@@ -70,7 +70,7 @@ Page({
             return ;
         }
         wx.request({
-            url: 'https://' + app.globalData.apiUrl + '?m=home&c=User&a=setInvite&id=' + that.data.userInfo.ID + "&invited=" + that.data.invite,
+            url: 'https://' + app.globalData.apiUrl + '?m=home&c=User&a=setInvite&id=' + that.data.userInfo.ID + "&invited=" + that.data.invite + "&inviteId=" + that.data.id,
             method: "GET",
             dataType: "json",
             success: function (res) {
